01eac0915ce911682e1fc0298224f492d206009c
YaroslavnaYermolenko <162818983+YaroslavnaYermolenko@users.noreply.github.com>
2024-12-17 23:15:15 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,9 @@
-export default function Feedback(
-  feedback,
-  totalFeedback,
-  positiveFeedback
-) {
+export default function Feedback(feedback, totalFeedback, positiveFeedback) {
   return (
     <div>
       <ul>
         <li>Good: {feedback.good}</li>
+        <li>Neutral: {feedback.neutral}</li>
         <li>Bad {feedback.bad}</li>
         <li>Total: {totalFeedback}</li>
         <li>Positive: {positiveFeedback}%</li>
